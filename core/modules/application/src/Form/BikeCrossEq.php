c6eb0d5e68f18496ceb829f5cb58c86e52e71e69
qoolposan <alexazazaev@yandex.ru>
2016-12-22 18:47:34 +0300

    Bike Cross Form

---
+++ core/modules/application/src/Form/BikeCrossEq.php
@@ -0,0 +1,67 @@
+<?php
+/**
+ * @file
+ * Contains \Drupal\application\Form\RallyEq.
+ */
+
+namespace Drupal\application\Form;
+
+use Drupal\Core\Form\FormBase;
+use Drupal\Core\Form\FormStateInterface;
+
+class BikeCrossEq extends FormBase {
+
+    public function getFormId() {
+        return 'bike_cross_eq';
+    }
+
+    public function buildForm(array $form, FormStateInterface $form_state) {
+
+        $form['shaped_puncher'] = array(
+            '#type' => 'number',
+            '#title' => 'Дырокол фигурный',
+            '#required' => TRUE,
+            '#attributes' => array (
+                'min' => '0'
+            )
+        );
+
+        $form['safety_pins'] = array(
+            '#type' => 'number',
+            '#title' => 'Английские булавки',
+            '#required' => TRUE,
+            '#attributes' => array (
+                'min' => '0'
+            )
+        );
+
+        
+
+        $form['printing_stuff'] = array(
+            '#type' => 'textfield',
+            '#title' => 'Печатные материалы',
+            '#required' => TRUE
+        );
+
+        $form['something_else'] = array(
+            '#type' => 'textarea',
+            '#title' => 'Что-то еще'
+        );
+
+        return $form;
+    }
+
+    public function validateForm(array &$form, FormStateInterface $form_state) {
+    //    if (strlen($form_state->getValue('name')) < 5) {
+    //      $form_state->setErrorByName('name', $this->t('Name is too short.'));
+    //    }
+    }
+
+    public function submitForm(array &$form, FormStateInterface $form_state) {
+//        drupal_set_message($this->t('@one, @two', array(
+//            '@one' => $form_state->getValue('competition_eq'),
+//            '@two' => $form_state->getValue('secretary_eq')
+//        )));
+    }
+
+}
