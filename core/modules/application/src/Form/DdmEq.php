e511f7541ce26cd7fe1ed594ef327b237dd924af
qoolposan <alexazazaev@yandex.ru>
2016-12-22 21:48:13 +0300

    DDM Form

---
+++ core/modules/application/src/Form/DdmEq.php
@@ -0,0 +1,55 @@
+<?php
+/**
+ * @file
+ * Contains \Drupal\application\Form\RallyEq.
+ */
+
+namespace Drupal\application\Form;
+
+use Drupal\Core\Form\FormBase;
+use Drupal\Core\Form\FormStateInterface;
+
+class DdmEq extends FormBase {
+
+    public function getFormId() {
+        return 'ddm_eq';
+    }
+
+    public function buildForm(array $form, FormStateInterface $form_state) {
+
+        $form['megaphone'] = array(
+            '#type' => 'number',
+            '#title' => 'Мегафон',
+            '#required' => TRUE,
+            '#attributes' => array (
+                'min' => '0'
+            )
+        );
+
+          $form['bikes'] = array(
+            '#type' => 'checkbox',
+            '#title' => 'Велики'
+        );
+
+        $form['something_else'] = array(
+            '#type' => 'textarea',
+            '#title' => 'Что-то еще'
+        );
+
+        return $form;
+    }
+
+    public function validateForm(array &$form, FormStateInterface $form_state) {
+    //    if (strlen($form_state->getValue('name')) < 5) {
+    //      $form_state->setErrorByName('name', $this->t('Name is too short.'));
+    //    }
+    }
+
+    public function submitForm(array &$form, FormStateInterface $form_state) {
+//        drupal_set_message($this->t('@one, @two', array(
+//            '@one' => $form_state->getValue('competition_eq'),
+//            '@two' => $form_state->getValue('secretary_eq')
+//        )));
+    }
+
+}
