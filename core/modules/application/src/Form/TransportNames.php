157b2a8b5452c09af56e292f5b36337f72a6b0c5
qoolposan <alexazazaev@yandex.ru>
2017-02-26 01:02:36 +0300

    Tr0102

---
+++ core/modules/application/src/Form/TransportNames.php
@@ -0,0 +1,54 @@
+<?php
+/**
+ * @file
+ * Contains \Drupal\application\Form\LaptopsTec.
+ */
+
+namespace Drupal\application\Form;
+
+use Drupal\Core\Form\FormBase;
+use Drupal\Core\Form\FormStateInterface;
+
+class TransportName extends WrapperFormBase {
+
+    public function getFormId() {
+        return 'transport_name';
+    }
+    public function buildForm(array $form, FormStateInterface $form_state) {
+
+    $form['will_be_used'] = array(
+            '#type' => 'checkbox',
+            '#title' => 'Будет ли использовано'
+        );
+    
+    $form['sign_format'] = array(
+            '#type' => 'textfield',
+            '#title' => 'Формат подписи',
+            'table_name' => 'transport_name'
+        ); 
+
+    $form['neat_package'] = array(
+            '#type' => 'checkbox',
+            '#title' => 'Аккуратная упаковка'
+            'table_name' => 'transport_name'
+        );
+       
+
+        
+        return $form;
+    }
+
+//    public function validateForm(array &$form, FormStateInterface $form_state) {
+//    //    if (strlen($form_state->getValue('name')) < 5) {
+//    //      $form_state->setErrorByName('name', $this->t('Name is too short.'));
+//    //    }
+//    }
+//
+//    public function submitForm(array &$form, FormStateInterface $form_state) {
+////        drupal_set_message($this->t('@one, @two', array(
+////            '@one' => $form_state->getValue('competition_eq'),
+////            '@two' => $form_state->getValue('secretary_eq')
+////        )));
+//    }
+
+}
